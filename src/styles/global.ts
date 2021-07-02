21dc2ddc62d66c747837db684b048a5260b38d1f
MauroBraga <M@r202150016774>
2021-07-02 10:19:07 -0300

    Fonte do Google Fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -31,10 +31,20 @@ export const  GlobalStyle = createGlobalStyle`
     }
 
     //REM = 1 rem = 16px
+    //vM@030618191
 
     body {
         background: var(---background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body,  input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    
+    h1, h2, h3,  h4, h5, h6, strong {
+        font-weight: 600;
     }
 
     button {
